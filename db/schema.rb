301c965e094aef21d9621587b90b6d5755eb97a0
anil pak <anilpak35@gmail.com>
2021-01-17 11:50:26 +1100

    Rails Wiki Articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_17_100605) do
+ActiveRecord::Schema.define(version: 2021_01_17_001137) do
 
-    create_table "articles", force: :cascade do |t|
-      t.string "title"
-      t.text "content"
-      t.datetime "created_at", precision: 6, null: false
-      t.datetime "updated_at", precision: 6, null: false
-    end
-  
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
+
+end
